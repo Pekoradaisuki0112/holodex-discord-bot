39e63b2f2bbf70cbcc38f81c58877f1638d5a03f
Pekoradaisuki0112 <123238710+Pekoradaisuki0112@users.noreply.github.com>
2025-08-19 17:00:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,15 +22,13 @@ def build_embeds(live_streams, upcoming_streams):
     live_filtered = [s for s in live_streams if s["channel"]["id"] in CHANNELS]
     for s in live_filtered:
         stream_id = s["id"]
-        embed = {
-            "username": "Holodex Notifier",
-            "avatar_url": s["channel"]["photo"],  # 頻道頭像
+        embeds.append({
             "title": s["channel"]["name"],
             "description": f"[{s['title']}](https://youtu.be/{stream_id})",
             "color": 0xFF69B4,
-            "thumbnail": {"url": f"https://img.youtube.com/vi/{stream_id}/mqdefault.jpg"}
-        }
-        embeds.append(embed)
+            "thumbnail": {"url": f"https://img.youtube.com/vi/{stream_id}/mqdefault.jpg"},
+            "avatar_url": s["channel"]["photo"]
+        })
 
     # ⏰ 一小時後開播
     now = datetime.now(TWTZ)
@@ -41,20 +39,23 @@ def build_embeds(live_streams, upcoming_streams):
         start_time = datetime.fromisoformat(s["start_scheduled"].replace("Z","+00:00")).astimezone(TWTZ)
         if now <= start_time <= one_hour_later:
             stream_id = s["id"]
-            embed = {
-                "username": "Holodex Notifier",
-                "avatar_url": s["channel"]["photo"],  # 頻道頭像
+            embeds.append({
                 "title": s["channel"]["name"],
                 "description": f"[{s['title']}](https://youtu.be/{stream_id})",
                 "color": 0x00BFFF,
-                "thumbnail": {"url": f"https://img.youtube.com/vi/{stream_id}/mqdefault.jpg"}
-            }
-            embeds.append(embed)
+                "thumbnail": {"url": f"https://img.youtube.com/vi/{stream_id}/mqdefault.jpg"},
+                "avatar_url": s["channel"]["photo"]
+            })
 
     return embeds
 
 def send_discord(embeds):
+    # 每個 embed 的 avatar_url Discord webhook 不支援單獨設置
+    # 所以取第一個主播的頭像作為 webhook avatar_url
+    webhook_avatar = embeds[0]["avatar_url"] if embeds else "https://i.imgur.com/your-default-avatar.png"
     payload = {
+        "username": "Holodex Notifier",
+        "avatar_url": webhook_avatar,
         "embeds": embeds
     }
     requests.post(WEBHOOK_URL, json=payload)
